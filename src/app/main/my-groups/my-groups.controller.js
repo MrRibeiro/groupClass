7e8422471f68d87eb398a14981d020d24efa7316
MrRibeiro <resilva.rs33@gmail.com>
2017-12-14 03:30:02 -0200

    Iniciando grupos

--- src/app/main/my-groups/my-groups.controller.js
+++ src/app/main/my-groups/my-groups.controller.js
@@ -6,7 +6,30 @@
       .controller('MyGroupsController', MyGroupsController);
   
     /** @ngInject */
-    function MyGroupsController() {
+    function MyGroupsController($scope, $http) {
         var vm = this;
+
+        initDadosMenu();
+        function initDadosMenu(){
+          //função mockada
+          $http.get('https://pacific-stream-91568.herokuapp.com/user/home/3').then(
+            function(resposta){
+              $scope.dataHome = resposta.data.data;
+              console.log($scope.dataHome);
+            }, function(resposta){
+                console.log(resposta);
+            });
+        }
+
+        initGrupos();
+        function initGrupos(){
+            $http.get('https://pacific-stream-91568.herokuapp.com/user/3/groups').then(
+                function(resposta){
+                    $scope.grupos = resposta.data;
+                    console.log($scope.grupos);
+                }, function(resposta){
+                    console.log(resposta);
+                });
+        }
     }
 })();
